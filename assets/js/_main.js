95f67c78c2fb8eb77df81a64c6d28ac5efb58fd5
mhyeh <samuel991640@gmail.com>
2026-01-08 16:01:44 -0600

    update

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -16,12 +16,12 @@ let determineComputedTheme = () => {
   if (themeSetting != "system") {
     return themeSetting;
   }
-  return "light";
-  // return (userPref && userPref("(prefers-color-scheme: dark)").matches) ? "dark" : "light";
+  return (userPref && userPref("(prefers-color-scheme: dark)").matches) ? "dark" : "light";
 };
 
 // detect OS/browser preference
-const browserPref = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+// const browserPref = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const browserPref = 'light';
 
 // Set the theme on page load or when explicitly called
 let setTheme = (theme) => {
